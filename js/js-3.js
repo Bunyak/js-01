9a0c34c8e45fcd41f316bcac4286505909ab0796
Bunyak Andriy <19091997x@gmail.com>
2019-03-29 09:03:53 +0000

    js-3

--- js/js-3.js
+++ js/js-3.js
@@ -1,5 +1,4 @@
-//https://www.codewars.com/kata/training-js-number-3-basic-data-types-string/train/javascript
-
+https://www.codewars.com/kata/training-js-number-3-basic-data-types-string/train/javascript
 
 var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
 function Dad(){
@@ -18,11 +17,11 @@ function banana(){
 //answer some questions if you finished works above
 function answer1(){
   //the answer should be "yes" or "no"
-  return "yes";
+  return "no";
 }
 function answer2(){
   //the answer should be "yes" or "no"
-  return "yes";
+  return "no";
 }
 function answer3(){
   //the answer should be "yes" or "no"
